8f676dfe9f869aa5a5b84511fd5500e9cdfba305
Josh L. Espinoza <jol.espinoz@gmail.com>
2020-08-24 23:49:32 -0700

    Updated version

--- hive_networkx/__init__.py
+++ hive_networkx/__init__.py
@@ -33,7 +33,7 @@
 # =======
 # Version
 # =======
-__version__= "2020.08.03"
+__version__= "2020.08.24"
 __author__ = "Josh L. Espinoza"
 __email__ = "jespinoz@jcvi.org, jol.espinoz@gmail.com"
 __url__ = "https://github.com/jolespin/hive_networkx"
